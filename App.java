b839a9f4e06c56cae686e852664036e574014856
Andrey <andn666@mail.ru>
2023-04-03 15:42:47 +0300

    add Person

--- App.java
+++ App.java
@@ -1,11 +1,15 @@
-package abc;
+//package abc;
 
 class App {
 	
 	String userName;
 	
+	Person p;
+	
 	public static void main (String[] args) {
 		System.out.println("Hello ugly World!");
+		
+		p = new Person();
 		
 	}
 	
